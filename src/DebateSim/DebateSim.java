4e35d5220e178e73f5afbee0f819c8dbc60cc590
9674036-code <9674036@graniteschools.org>
2026-08-30 15:47:52 +0000

    developed basic probabilty structure

--- src/DebateSim/DebateSim.java
+++ src/DebateSim/DebateSim.java
@@ -1,5 +1,15 @@
+import java.util.Random;
+
 public class DebateSim {
+    public static ArrayList<Double> judge = new ArrayList<>();
     public static void main(String[] args) {
-        
+        while (true){
+            for (int r=0;r<11;r++){
+                judge.add(random.nextInt(10,65));
+            }
+        }
+    }
+    public static int genJudge(int prob1,int prob2){
+        judge.add(random.nextInt(10,65));
     }
 }
