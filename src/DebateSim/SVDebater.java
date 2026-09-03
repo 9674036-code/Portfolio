1d921a45cb6a7abfa4e62cfe212ec33724fb6eb1
9674036-code <9674036@graniteschools.org>
2026-09-03 14:15:07 -0600

    Update SVDebater.java

--- src/DebateSim/SVDebater.java
+++ src/DebateSim/SVDebater.java
@@ -1,6 +1,12 @@
 public class SVDebater extends Debater{
     public static final int SPEAKER_I=80;
+    public SVDebater(int w,int p) {
+        setMod(27);
+        setWins(w);
+        setSpeakerP(p);
+    }
+    @Override
     public void addPerks(){
-        setValue(getValue()+6);
+        setValue(getValue()+getMod());
     }
 }
