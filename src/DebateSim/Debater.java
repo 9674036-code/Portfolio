cebebc6292187dee0d5808545df2f5f483f1b947
9674036-code <9674036@graniteschools.org>
2026-09-03 14:14:20 -0600

    Add mod field and update setter methods in Debater

--- src/DebateSim/Debater.java
+++ src/DebateSim/Debater.java
@@ -4,29 +4,35 @@ public class Debater{
     private int mainArg=0;
     private int value=0;
     private int crossX=0;
-
+    private int mod=0;
     
     public void reset(){
         speakerP=0;
         wins=0;
     }
-    public void setCrossX(int crossX) {
-        this.crossX = crossX;
+    public void setMod(int m){
+        this.mod=m;
     }
-    public void setMainArg(int mainArg) {
-        this.mainArg = mainArg;
+    public void setCrossX(int c) {
+        this.crossX = c;
+    }
+    public void setMainArg(int m) {
+        this.mainArg = m;
     }
     public void setSpeakerP(int s) {
         speakerP = s+speakerP;
     }
-    public void setValue(int value) {
-        this.value = value;
+    public void setValue(int v) {
+        this.value = v;
     }
     public void setWins(int w) {
         wins = w+wins;
     }
     public int getCrossX() {
         return crossX;
+    }
+    public int getMod(){
+        return mod;
     }
     public int getMainArg() {
         return mainArg;
@@ -39,5 +45,8 @@ public class Debater{
     }
     public int getWins() {
         return wins;
+    }
+    public void addPerks(){
+        
     }
 }
