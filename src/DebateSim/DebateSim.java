214517cd4dca7e33fa98819c448a2f23281834db
9674036-code <9674036@graniteschools.org>
2026-09-03 14:14:00 -0600

    Refactor player to use ArrayList of Debater objects

--- src/DebateSim/DebateSim.java
+++ src/DebateSim/DebateSim.java
@@ -9,8 +9,11 @@ public class DebateSim {
     public static ArrayList<Integer> judge = new ArrayList<>();
     public static String[] iCalc ={"I like seeing clash and impact calc.","Clash and impact calc are very important for me.","Impact calc is how debate works and will win/lose you the round."};
     public static String[] value={"The value debate is pretty fundamental but not really extensive so I'll be a judging a bit of the round on it.","The value debate is quite fundamental so I'll weigh it considerably.","Sure impact calc is important, but the value debate is cool so I will weigh most of the round on it."};
-    public static Debater player = new Debater();
-    public static Debater opp = new Debater();
+    public static ArrayList<Debater> player = new ArrayList<>();
+    int playerType=0;
+    player.add(new ExtinctionDebater(0,0));
+
+    public static Debater opp = new ExtinctionDebater(0,0);
     public static void main(String[] args) {
         while (true){
             for (int r=0;r<5;r++){
@@ -50,12 +53,12 @@ public class DebateSim {
                 while(true){    
                     try{
                         System.out.println("How many points out of 100 of prepatory time do you wish to allocate to your value framework? (you will be splitting these points three ways) ");
-                        player.setValue(Integer.parseInt(input.nextLine()));
+                        player.get(playerType).setValue(Integer.parseInt(input.nextLine()));
                         System.out.println("How many points out of 100 of prepatory time do you wish to allocate towards Cross Examination? (you will be splitting these points three ways) ");
-                        player.setCrossX(Integer.parseInt(input.nextLine()));
+                        player.get(playerType).setCrossX(Integer.parseInt(input.nextLine()));
                         System.out.println("How many points out of 100 of prepatory time do you wish to allocate to your main argument and impact calculus? (you will be splitting these points three ways) ");
-                        player.setMainArg(Integer.parseInt(input.nextLine()));
-                        if(player.getCrossX()+player.getMainArg()+player.getValue()==100){
+                        player.get(playerType).setMainArg(Integer.parseInt(input.nextLine()));
+                        if(player.get(playerType).getCrossX()+player.get(playerType).getMainArg()+player.get(playerType).getValue()==100){
                         break;
                         }
                         System.out.println("please use a valid integer and ensure your inputted values sum to 100");
@@ -63,15 +66,16 @@ public class DebateSim {
                         System.out.println("please use a valid integer and ensure your inputted values sum to 100");
                     }
                 }
+                player.get(playerType).addPerks();
                 
                 System.out.println("Judge importance ratings (out of 100)");
                 System.out.println("The main arguments/impact calc :" +judge.get(0));
                 System.out.println("The value debate: " +judge.get(1));
                 System.out.println("Cross Examination: " +judge.get(2));
 
-                if(effectCalc(player.getCrossX(),judge.get(2)+effectCalc(player.getMainArg(),judge.get(0))+effectCalc(player.getValue(),judge.get(1)))<effectCalc(opp.getCrossX(),judge.get(2))+effectCalc(opp.getMainArg(),judge.get(0))+effectCalc(opp.getValue(),judge.get(1))){
+                if(effectCalc(player.get(playerType).getCrossX(),judge.get(2)+effectCalc(player.get(playerType).getMainArg(),judge.get(0))+effectCalc(player.get(playerType).getValue(),judge.get(1)))<effectCalc(opp.getCrossX(),judge.get(2))+effectCalc(opp.getMainArg(),judge.get(0))+effectCalc(opp.getValue(),judge.get(1))){
                     System.out.println("You have won the round.");
-                    player.setWins(1);
+                    player.get(playerType).setWins(1);
                 }else{
                     System.out.println("You have lost the round.");
                 }
@@ -79,7 +83,7 @@ public class DebateSim {
                 judge.clear();
                 paradigm="Hello, if you are reading this, I am probably your judge, here's what I'm looking for: ";
             }
-        switch(player.getWins()){
+        switch(player.get(playerType).getWins()){
             case 4:
                 System.out.println("You placed "+random.nextInt(2)+4+"th");
             case 5:
@@ -93,7 +97,7 @@ public class DebateSim {
             input.close();
             break;
         }
-        player.reset();
+        player.get(playerType).reset();
         }
     }
     public static int effectCalc(int j, int p){
