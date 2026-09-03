f8c389a85e4264bd2a2223b94582793ec2d49f30
9674036-code <9674036@graniteschools.org>
2026-09-03 14:14:50 -0600

    Add constructor and modify addPerks method

--- src/DebateSim/ProgDebater.java
+++ src/DebateSim/ProgDebater.java
@@ -1,6 +1,12 @@
 public class ProgDebater extends Debater{
     public static final int SPEAKER_I=50;
+    public ProgDebater(int w,int p) {
+        setMod(18);
+        setWins(w);
+        setSpeakerP(p);
+    }
+    @Override
     public void addPerks(){
-        setMainArg(getMainArg()+27);
+        setValue(getValue()+getMod());
     }
 }
