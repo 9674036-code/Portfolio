489f045f7714815f123cc52dedbe068d04f3e4e9
9674036-code <9674036@graniteschools.org>
2026-09-03 14:14:35 -0600

    Add constructor and modify addPerks method

--- src/DebateSim/ExtinctionDebater.java
+++ src/DebateSim/ExtinctionDebater.java
@@ -1,6 +1,12 @@
 public class ExtinctionDebater extends Debater{
     public static final int SPEAKER_I=20;
+    public ExtinctionDebater(int w, int p) {
+        setMod(31);
+        setWins(w);
+        setSpeakerP(p);
+    }
+    @Override
     public void addPerks(){
-        setMainArg(getMainArg()+31);
+        setMainArg(getMainArg()+getMod());
     }
 }
